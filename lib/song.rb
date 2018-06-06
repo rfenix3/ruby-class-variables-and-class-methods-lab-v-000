685af72478fc04ff80fd5e9f2008f9e0140863ed
Richard Fenix <richard.fenix@gmail.com>
2018-06-06 13:38:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,8 +45,8 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do {|artist|
-    if artist_count[artist]
-      artist_count[artist] += 1 
+    if artist_count[artist]      # if artist already exist in the hash
+      artist_count[artist] += 1  # add 1 to the artist's song count  
       
     }
     
